a513ca06eaec953f50e615882603780dbc91a000
georgype <74106887+georgype@users.noreply.github.com>
2021-07-23 13:01:43 +0300

    Delete task_2.py

--- lesson_2/task_2.py
+++
@@ -1,8 +0,0 @@
-a = input('Введите элемент массива (для завершения ввода оставте строку пустой) ')
-my_list =[]
-while a:
-    my_list.append(a)
-    a = input('Введите элемент массива (для завершения ввода оставте строку пустой) ')
-for i in range(1, len(my_list), 2):
-    my_list[i], my_list[i-1] = my_list[i-1], my_list[i]
-print(my_list)
